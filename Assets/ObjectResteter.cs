fdf0a036088161c8d854d8885ef0449126618e1c
PATIL2007 <patrycja@lenik.eu>
2023-06-14 16:44:56 +0200

    reseting objects

--- Assets/ObjectResteter.cs
+++ Assets/ObjectResteter.cs
@@ -6,17 +6,30 @@ public class ObjectResteter : MonoBehaviour
 {
     public Transform transformToReset;
     public Vector3 startingPosition;
-    public Vector3 startingRotation;
+    public Vector3 startingRotationAngles;
+    public Quaternion startingRotation; 
     // Start is called before the first frame update
     void Start()
     {
+        if(transformToReset == null)
+        {
+            transformToReset = transform;
+        }
+        
         startingPosition = transformToReset.position;
-       // startingRotation = transformToReset.rotation;
+        startingRotation = transformToReset.rotation;
+        startingRotationAngles = startingRotation.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("jestok");
+            transformToReset.position = startingPosition;
+            startingRotation.eulerAngles = startingRotationAngles;
+            transformToReset.rotation = startingRotation;
+        }
     }
 }
